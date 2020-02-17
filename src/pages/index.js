394d4baed73d469fa2eb41266afa7b9dcd96cd0f
Randy Ferrer <randfer@gmail.com>
2020-02-17 11:41:54 -0500

    Basic scatterplot

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as D3 from 'd3';
+import { useD3 } from 'd3blackbox';
+
+// .range gives you an empty array of x number of elements
+const getRandomData = () =>
+  D3.range(20).map(_ => ({ x: Math.random(), y: Math.random() }));
+
+const Axis = ({ x, y, scale, axisType }) => {
+  const fnName = axisType === 'left' ? 'axisLeft' : 'axisBottom';
+  const ref = useD3(el => D3.select(el).call(D3[fnName](scale)));
+
+  return <g transform={`translate(${x}, ${y})`} ref={ref} />;
+};
+
+export default () => {
+  const data = getRandomData();
+  const width = 400;
+  const height = 400;
+
+  const xScale = D3.scaleLinear()
+    .domain([0, 1])
+    .range([45, width - 10]);
+
+  const yScale = D3.scaleLinear()
+    .domain([0, 1])
+    .range([height - 45, 5]); // flip this to flip the range
+
+  return (
+    <svg width={width} height={height}>
+      {/* create a scatterplot of the actual data */}
+      {data.map(d => (
+        <circle cx={xScale(d.x)} cy={yScale(d.y)} r={5} />
+      ))}
+      <Axis x={40} y={0} scale={yScale} axisType="left" />
+      <Axis x={0} y={height - 40} scale={xScale} axisType="bottom" />
+    </svg>
+  );
+};
